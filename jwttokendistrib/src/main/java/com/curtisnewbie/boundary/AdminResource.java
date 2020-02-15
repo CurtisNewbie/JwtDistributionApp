e3a8ffd8e3fadf7a35f99bca3e0bafe8b0f5993d
CurtisNewbie <zhuangyongj@gmail.com>
2020-02-15 23:26:59 +0000

    Impl REST endpoints to authenticate using BASIC and distribute JWT
    Administrators use the BASIC authentication to verify their identifies,
    once authenticated, they are responded with generated JWT.

---
+++ jwttokendistrib/src/main/java/com/curtisnewbie/boundary/AdminResource.java
@@ -0,0 +1,34 @@
+package com.curtisnewbie.boundary;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.curtisnewbie.authentication.Authenticator;
+
+@Path("admin")
+@RequestScoped
+public class AdminResource {
+
+    @Inject
+    private Authenticator auth;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getToken(@HeaderParam("authorization") String basicAuthStr) throws Exception {
+        if (basicAuthStr == null)
+            return Response.status(401).build();
+
+        String[] nameAndPw = auth.decode(basicAuthStr);
+        if (auth.isAuthenticated(nameAndPw[0], nameAndPw[1]))
+            return Response.ok(auth.generateJWT(nameAndPw[0])).build();
+        else
+            return Response.status(401).build();
+    }
+
+}
