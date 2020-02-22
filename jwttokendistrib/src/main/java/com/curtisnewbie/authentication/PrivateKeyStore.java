16720485d457ab441a261bb7a7c2b1c394e9e543
CurtisNewbie <zhuangyongj@gmail.com>
2020-02-22 17:17:56 +0000

    Remove config.txt and use MP-Config to load private key
    MP-Config is a much more reliable way to inject the configuration properties.

--- jwttokendistrib/src/main/java/com/curtisnewbie/authentication/PrivateKeyStore.java
+++ jwttokendistrib/src/main/java/com/curtisnewbie/authentication/PrivateKeyStore.java
@@ -1,32 +1,20 @@
 package com.curtisnewbie.authentication;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Singleton
 @Startup
 public class PrivateKeyStore {
 
+    @Inject
+    @ConfigProperty(name = "jwt_private_key")
     private String privateKey;
-
-    @PostConstruct
-    public void init() throws IOException {
-        this.privateKey = loadPrivateKey();
-    }
 
     public String getPrivateKeyStr() {
         return this.privateKey;
-    }
-
-    private String loadPrivateKey() throws IOException {
-        var in = this.getClass().getClassLoader().getResourceAsStream("config.txt");
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in));) {
-            return reader.readLine().substring(12);
-        }
     }
 }
