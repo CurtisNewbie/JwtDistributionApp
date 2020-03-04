af715b3e687b54c9dbc9628cc4de705ab7c48f82
CurtisNewbie <zhuangyongj@gmail.com>
2020-03-04 17:54:09 +0000

    Add import.sql and config in mp-config.properties for demo

---
+++ jwttokendistrib/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+-----------------------------------
+
+-- Admin Credentials, only use it for demo purpose
+
+-----------------------------------
+
+-- username: apple, password (SHA256): juice, hash: 1234
+INSERT INTO admin VALUES ('apple', 'acdb4ae487227ba3ac329e58a2c250673951e6ce7994c4c4bceeaa7ad6d42cb6', '1234');
